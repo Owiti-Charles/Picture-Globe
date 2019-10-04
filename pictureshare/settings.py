a09341cae65bdbc48906e9a915e05fe9fb7b3685
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-04 16:31:52 +0300

    Add database configuration to settings

--- pictureshare/settings.py
+++ pictureshare/settings.py
@@ -89,7 +89,8 @@ DATABASES = {
             'HOST': config('DB_HOST'),
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
