93a00cc18c12223df98e91c5203d2a1d06262a70
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-04 17:56:39 +0300

    create location and categories model

--- pictures/models.py
+++ pictures/models.py
@@ -17,4 +17,27 @@ class Image(models.Model):
         self.delete()
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
