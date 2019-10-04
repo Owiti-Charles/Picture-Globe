0624df8214c70e07a429315f8317969966175637
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-04 17:18:34 +0300

    Create a base layout templates that all other templates would inherit from

--- pictureshare/urls.py
+++ pictureshare/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('pictures.urls'))
+    url(r'', include('pictures.urls'))
 ]
