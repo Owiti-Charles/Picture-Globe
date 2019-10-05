d1ecba152e2cdb427bbd78cbbf5526a350c5b7f3
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-05 13:19:37 +0300

    Update images by uploading more images

--- pictures/views.py
+++ pictures/views.py
@@ -5,4 +5,4 @@ from .models import Image
 def index(request):
     images = Image.objects.all()
     print (images)
-    return render(request, 'pictures/index.html', {'images': images})
+    return render(request, 'pictures/index.html', {'images': images[::-1]})
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,3 @@
-copyLink1 = ()=>{
-document.getElementById("imagelink").select();
-document.execCommand("Copy");
-}
 copyLink =(element) => {
      document.getElementById(element).select();
      document.execCommand("copy");
