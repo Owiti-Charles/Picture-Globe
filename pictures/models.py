8fd7a96433bea81a8b97b73ce8ed152975d06f47
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-05 16:50:20 +0300

    Add views method to get images based on location

--- pictures/models.py
+++ pictures/models.py
@@ -17,6 +17,11 @@ class Category(models.Model):
 class Location(models.Model):
     name = models.CharField(max_length=60)
 
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
     def __str__(self):
         return self.name
 
@@ -37,8 +42,18 @@ class Image(models.Model):
     location = models.ForeignKey(Location)
 
     @classmethod
-    def search_by_category(cls, search_term):
-        images = cls.objects.filter(category__name__icontains=search_term)
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location)
+        return image_location
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.Image.filter(id=id)
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
         return images
 
     def __str__(self):
--- pictures/views.py
+++ pictures/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
-from .models import Image
+from .models import Image, Location
 
 
 def index(request):
     images = Image.objects.all()
-    print(images)
-    return render(request, 'pictures/index.html', {'images': images[::-1]})
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'pictures/index.html', {'images': images[::-1], 'locations':locations})
+
+
+def image_location(request, location):
+    images = Image.filter_by_location()
+
+    return render(request, 'pictures/index.html', {'ima': images})
 
 
 def search_results(request):
     if 'imagesearch' in request.GET and request.GET["imagesearch"]:
-        search_term = request.GET.get("imagesearch")
-        searched_images = Image.search_by_category(search_term)
-        message = f"{search_term}"
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
         print(searched_images)
         return render(request, 'pictures/search_results.html', {"message": message, "images": searched_images})
     else:
