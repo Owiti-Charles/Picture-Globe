bc15b77616e0f963d5a8f8799b27cab233b44aa9
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-05 17:08:49 +0300

    Update tests

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Image, Category, Location
+
+
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(name='Moringa')
+        self.location.save_location()
+
+        self.image_test = Image(id=1, name= 'image', description='this is a test image', location=self.location)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
