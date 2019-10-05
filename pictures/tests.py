6e0d81f6f8f68c43b6b1ab4842d5c66ea409f99a
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-05 22:02:51 +0300

    Add test for saving an image

--- pictures/tests.py
+++ pictures/tests.py
@@ -8,7 +8,16 @@ class TestImage(TestCase):
         self.location = Location(name='Moringa')
         self.location.save_location()
 
-        self.image_test = Image(id=1, name= 'image', description='this is a test image', location=self.location)
+        self.category = Category(name='home')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='image', description='this is a test image', location=self.location,
+                                category=self.category)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.image_test, Image))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
